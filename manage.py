ee8d7ecf1daadd5843fbd5a2b55d913a0c9dafd0
rwspatin <rwspatin@gmail.com>
2021-03-21 17:11:16 -0300

    COrrect port

--- manage.py
+++ manage.py
@@ -12,7 +12,8 @@ manager = Manager(app)
 
 @manager.command
 def run():
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
     manager.run()
